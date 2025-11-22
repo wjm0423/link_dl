f7dd60b0622e3922a734a52a88243b8bfd47de64
wjm0423 <wjm0423@naver.com>
2025-11-22 23:39:53 +0900

    update

--- _01_code/_09_fcn_best_practice/d_tester.py
+++ _01_code/_09_fcn_best_practice/d_tester.py
@@ -47,6 +47,7 @@ class ClassificationTester:
     with torch.no_grad():
       input_test = self.transforms(input_test)
       input_test = input_test.unsqueeze(dim=0)
+      input_test = input_test.to(torch.device('cpu'))
 
       output_test = self.model(input_test)
       predicted_test = torch.argmax(output_test, dim=-1)
